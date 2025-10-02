bc5e02be33a420a0b903d1a411d192ce36b4d44f
Harshit Mishra <harshitmishra.84162@gmail.com>
2025-10-02 20:55:38 +0530

    initial commit

---
+++ posts/views.py
@@ -0,0 +1,12 @@
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+
+@api_view(http_method_names=["GET"])
+def homepage(request: Request):
+    response={"message":"this is homepage"}
+    return Response(data=response,status=status.HTTP_200_OK)
+    
+    
