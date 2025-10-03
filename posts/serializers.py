688310481af7e3aae7406b01403013946acfe8dd
Harshit Mishra <harshitmishra.84162@gmail.com>
2025-10-03 23:53:04 +0530

    latest commit

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
+from .models import Post
 
 
+# class PostSerializer(serializers.Serializer):   #manual serializer
+#     id=serializers.IntegerField(read_only=True)
+#     title=serializers.CharField(max_length=50)
+#     content=serializers.CharField()
+#     created=serializers.DateTimeField(read_only=True)
 
-class PostSerializer(serializers.Serializer):
-    id=serializers.IntegerField(read_only=True)
-    title=serializers.CharField(max_length=50)
-    content=serializers.CharField()
-    created=serializers.DateTimeField(read_only=True)
+
+class PostSerializer(serializers.ModelSerializer):  #model based serializer
+    class Meta:
+        model=Post
+        fields=["id","title","content","created"]
+        read_only_fields=["id","created"]
+        
+        
--- posts/views.py
+++ posts/views.py
@@ -2,13 +2,17 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.views import APIView
 
 
-posts=[
-    {"id":1,"title":"First Post","content":"This is the content of the first post."},
-    {"id":2,"title":"Second Post","content":"This is the content of the second post."},
-    {"id":3,"title":"Third Post","content":"This is the content of the third post."}
-]
+
+# posts=[
+#     {"id":1,"title":"First Post","content":"This is the content of the first post."},
+#     {"id":2,"title":"Second Post","content":"This is the content of the second post."},
+#     {"id":3,"title":"Third Post","content":"This is the content of the third post."}
+# ]
 
 
 
@@ -17,30 +21,52 @@ posts=[
 
 @api_view(http_method_names=["GET","POST"])
 def homepage(request: Request):
+    pass
 
+#     if request.method=="POST":
+#         data=request.data
+#         response={"message":"this is homepage","data":data}
+#         return Response(data=response,status=status.HTTP_201_CREATED)
+        
+#     response={"message":"this is homepage"}
+#     return Response(data=response,status=status.HTTP_200_OK)
+
+
+
+
+@api_view(http_method_names=["GET","POST"])
+def list_posts(request: Request):
+    posts=Post.objects.all()
+    
+    
     if request.method=="POST":
         data=request.data
-        response={"message":"this is homepage","data":data}
-        return Response(data=response,status=status.HTTP_201_CREATED)
         
-    response={"message":"this is homepage"}
+        serializer=PostSerializer(data=data)
+        
+        if serializer.is_valid():
+            serializer.save()  # saves the data to the database
+            response={"message":"post created successfully","data":serializer.data}
+            return Response(data=response,status=status.HTTP_201_CREATED)
+    
+        return Response(data=serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    serializer=PostSerializer(instance=posts,many=True)  # here many=True is used to serialize multiple objects
+    
+    
+    response={"message":"list of posts","data":serializer.data}   # format the response in this way
+    
+    
     return Response(data=response,status=status.HTTP_200_OK)
-
-
-
-
-@api_view(http_method_names=["GET"])
-def list_posts(request: Request):
-
-    return Response(data=posts,status=status.HTTP_200_OK)
 
 
 @api_view(http_method_names=["GET"])
 def post_detail(request: Request,post_index:int):
-    post=posts[post_index]
+    pass
+#     post=posts[post_index]
     
-    if post:
-        return Response(data=post,status=status.HTTP_200_OK)
+#     if post:
+#         return Response(data=post,status=status.HTTP_200_OK)
     
-    return Response(data={"message":"post not found"},status=status.HTTP_404_NOT_FOUND)
+#     return Response(data={"message":"post not found"},status=status.HTTP_404_NOT_FOUND)
     
